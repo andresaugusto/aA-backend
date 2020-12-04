7256a1a133d04ec3b82a82c75445eb33d8f681cf
andresaugusto <andres.augusto@icloud.com>
2020-12-04 11:35:56 -0500

    changes connection variables to double quotes

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
 const mongoURI = 
-    process.env.NODE_ENV === 'production' 
+    process.env.NODE_ENV === "production" 
     ? process.env.MONGODB_URI 
-    : 'mongodb://localhost/aa-portfolio'
+    : "mongodb://localhost/aa-portfolio"
 
 mongoose.connect(mongoURI, {
-    useFindAndModify: false, //ADDED JIC
-    useCreateIndex: true, //ADDED JIC
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false, //ADDED JIC
+    useCreateIndex: true //ADDED JIC
 })
 .then((instance) => 
 console.log(`CONECTAO AL DB: ${instance.connections[0].name}`)
